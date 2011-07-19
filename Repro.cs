3a01d6a9d43538cbb53df7e24be80824984cb29e
Omer Mor <omer.mor@gmail.com>
2011-07-19 21:40:24 +0300

    added repro solution

---
+++ Repro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Resharper6Bug
+{
+    public class Repro
+    {
+        public void Foo()
+        {
+            IObservable<int> source = null;
+
+            // Type the following, character by character:
+            //
+            //    source.Select(
+            //
+
+            //
+            // When entering the '(' character (or when letting R# auto-complete), 
+            // R# will reformat the statement to static method invocation:
+            //
+            //    Observable.Select(source)
+            //
+        }
+    }
+}
